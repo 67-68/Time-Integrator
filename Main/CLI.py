5ce25aba501934234f5178fc230e7373786535bb
6768 <amongus6768@icloud.com>
2025-06-10 20:30:14 +0800

    写了展示功能 展示所有行动和具体时间，占比

--- Main/CLI.py
+++ Main/CLI.py
@@ -3,41 +3,71 @@ from parseInput import inputTimespan, parseDataIntoList
 from save_and_load import firstSaveData, getData, saveData
 
 def promptInput():
-    # use a while loop, rather than a function to prompt input
+    #input the data
+    userData = input("input your data")
+    date = input("what date is it today? please enter in the form of 2025/6/10")
+    
+    #TODO:presence check
+    
+    #TODO:use strip to delete spaces
+    
+    #TODO:change it into discard redundant space in data
+    
+    #change the format of the data, in order to save into json
+    userData = userData.split(" - ")
+    
+    #put data into the list
+    actions = parseDataIntoList(userData)
+    
+    #Format check and change the format
+    for i in range (len(actions)):
+        actions[i]["end"] = formatValidation(actions[i]["start"],actions[i]["end"])
+    
+    #validate whether they are reasonable
+    if rangeValidation(actions) == True:
+        print("pass range validation correctly")
+    
+    #输入timespan
+    inputTimespan(actions)
+    
+    #把actions包裹进一个新的dictionary
+    data = {date : actions}
+    
+    #检测是否data为空，然后选择不同的输入function
+    temp = getData()
+    if not temp:
+        firstSaveData(data)
+    else:
+        saveData(date,actions)
+ 
+def mainMenu():
     while True:
-        #input the data
-        userData = input("input your data")
-        date = input("what date is it today? please enter in the form of 2025/6/10")
-        
-        #TODO:presence check
-        
-        #TODO:use strip to delete spaces
-        
-        #TODO:change it into discard redundant space in data
-        
-        #change the format of the data, in order to save into json
-        userData = userData.split(" - ")
-        
-        #put data into the list
-        actions = parseDataIntoList(userData)
-        
-        #Format check and change the format
-        for i in range (len(actions)):
-            actions[i]["end"] = formatValidation(actions[i]["start"],actions[i]["end"])
-        
-        #validate whether they are reasonable
-        if rangeValidation(actions) == True:
-            print("pass range validation correctly")
-        
-        #输入timespan
-        inputTimespan(actions)
-        
-        #把actions包裹进一个新的dictionary
-        data = {date : actions}
-        
-        #检测是否data为空，然后选择不同的输入function
-        temp = getData()
-        if not temp:
-            firstSaveData(data)
-        else:
-            saveData(date,actions)
+        choice = input("enter input to input, quit to quit, show to enter next menu")
+        if (choice == "input"):
+            promptInput()
+        elif choice == "quit":
+            break
+        elif choice == "show":
+            demonstration()
+
+#新建一个活动为大分类的字典，遍历整个字典，整理活动
+def demonstration():
+    #获取基本的数据
+    data = getData()
+    
+    #新建需要的变量
+    actions = {}
+    totalTime = 0
+    
+    
+    #把数据输出
+    for date in data: #输出每一天
+        for action_dict in data[date]: #遍历每一天的每个字典,输出类似a{"A":1,"B":2}
+            action_str = action_dict["action"]
+            if action_str not in actions:
+                actions[action_str] = 0 #暂时先把它当作总时长，不管细节
+            actions[action_str] += action_dict["timespan"]
+            totalTime += action_dict["timespan"]
+    
+    for action_str in actions:
+        print(f"action:{action_str} timespan:{actions[action_str]} {(actions[action_str]/totalTime)*100}% of total time")
--- Main/main.py
+++ Main/main.py
@@ -1,18 +1,15 @@
-"""
-我暂时的思路就是在其他的py文件里面存universal的function，至于把它们运用起来就放在main，也就是说validation也要在main
-首先使用while输入数据,把它转换为通用的json格式
-然后validate,如果validate = true 就存进里面
-由于是按天数存放的，不可能出现两天一样的情况
 
-额外信息：可能需要在每一步加上debug信息，但那是后面的事情了
-"""
 import json
-from CLI import promptInput
+from CLI import mainMenu, promptInput
 
 #Main function start
 print("welcome to the time-integrater, it is a gadget that helps you to analyze your time distribusion")
 
-promptInput()
+#menu start
+mainMenu()
+
+
+        
         
     
     
